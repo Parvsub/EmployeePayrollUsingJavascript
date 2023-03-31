db0ea35817025e7a4679d8e94441f6999b5f49b2
Parvsub <Praveenboda421@gmail.com>
2023-03-31 13:11:41 +0530

    Added: Using RegEx for name and throw error

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -14,22 +14,31 @@ class EmployeePayroll{
         this.startDate = params[4];
     }
     //getter and setter method
-    get name(){return this.name;}
-    set name(name){this._name = name;}
+    get name(){return this._name;}
+    set name(name){
+        let nameRegix = RegExp ('^[A-Z]{1}[a-z]{3,}$');
+        if(nameRegix.test(name))
+        this._name = name;
+        else throw 'Name is Incorrect!';
+    }
 
     //method
     toString(){
         const options = { year : 'numeric',month: 'long', day: 'numeric'};
-        const employeeDate = this.startDate === undefined ? "undefined":
+        const employeeDate = this.startDate ?"undefined":
         this.startDate.toLocalDateString("en-US", options);
-        return "id=" + this.id +",name='" + this._name +",salary="+this.salary+","+
-        "gender"+this.gender+",startDate="+employeeDate; 
+        return "id=" + this.id +",name='" + this.name +",salary="+this.salary+","+
+        "gender="+this.gender+",startDate="+employeeDate; 
     }
 }
 
 let employeePayroll = new EmployeePayroll(1,"Mark",30000);
 console.log(EmployeePayroll.toString());
-EmployeePayroll.name ="Mohan";
+try{
+EmployeePayroll.name ="Praveen";
 console.log(EmployeePayroll.toString());
+}catch(e){
+    console.error(e);
+}
 let newEmployeePayroll = new EmployeePayroll(1,"Terrisa",30000,"F",new Date());
 console.log(newEmployeePayroll.toString());
